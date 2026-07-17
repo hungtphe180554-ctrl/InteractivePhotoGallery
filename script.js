36930ad6a3ea4a7f4f905f6ad64e7ffdbe78eaa0
hungtphe180554-ctrl <hungtphe180554@fpt.edu.vn>
2026-07-17 17:06:01 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function upDate(previewPic){
+
+    console.log(previewPic);
+
+    console.log(previewPic.alt);
+
+    console.log(previewPic.src);
+
+    document.getElementById("image").innerHTML = previewPic.alt;
+
+    document.getElementById("image").style.backgroundImage =
+        "url('" + previewPic.src + "')";
+
+}
+
+function unDo(){
+
+    document.getElementById("image").style.backgroundImage = "url('')";
+
+    document.getElementById("image").innerHTML =
+        "Hover over an image below to display here.";
+
+}
